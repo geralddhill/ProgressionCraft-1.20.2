a24dcfcbc64788aa9b9eccc47b359a4ce4ad4245
Gerald <gerald@Geralds-MacBook-Pro.local>
2023-10-27 18:40:21 -0700

    Initial commit

---
+++ src/main/java/com/getblockt/progressioncraft/ProgressionCraftClient.java
@@ -0,0 +1,10 @@
+package com.getblockt.progressioncraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProgressionCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
